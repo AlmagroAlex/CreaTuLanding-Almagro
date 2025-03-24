d01f09c5f3004a775ceaf6e9575ca95ed7bf0640
Alex <almagroalex14@gmail.com>
2025-03-24 14:08:02 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Selecciona el botón por su ID
+const boton = document.getElementById("miBoton");
+
+// Añade un evento que muestra un mensaje cuando se hace clic
+boton.addEventListener("click", () => {
+  alert("¡Has hecho clic en el botón!");
+});
